db4951e41516d3805164d26f7d8a5b646a8ddd80
theaisforannie <annharter@gmail.com>
2015-06-01 21:24:46 -0400

    whoops i had a bunch of stuff in flattery.rb that wasn't running, i hope i fixed that!

--- flattery.rb
+++ flattery.rb
@@ -1,32 +1,6 @@
 require 'sinatra'
 require 'yaml'
-require 'slim'
 
-
-# get '/hi' do
-# 	YAML.load_file("compliments.yml").sample
-# end
-
-get '/' do
-	slim :index
+get '/hi' do
+	YAML.load_file("compliments.yml").sample
 end
-
-__END__
-
-@@layout
-doctype html
-html
-  head
-    meta charset="utf-8"
-    title Flatter Me 
-    link rel="stylesheet" media="screen, projection" href="/styles.css"
-    /[if lt IE 9]
-      script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
-  body
-    h1 YAML.load_file("compliments.yml").sample
-    == yield
-
-@@index
-h2 you are beautiful
-ul.tasks
-  li YAML.load_file("compliments.yml").sample
